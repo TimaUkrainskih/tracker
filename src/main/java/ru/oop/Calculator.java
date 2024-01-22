3e5bba169dcd61bd259f3d8f9e512f2270d74b97
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-22 23:04:04 +0300

    7.12 Статические и нестатические методы

---
+++ src/main/java/ru/oop/Calculator.java
@@ -0,0 +1,34 @@
+package ru.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return x - y;
+    }
+
+    public double divide(int y) {
+        return (double) x / y;
+    }
+
+    public double sumAllOperation(int y) {
+        return sum(y) + minus(y) + divide(y);
+    }
+
+    public static void main(String[] args) {
+        int result = Calculator.sum(10);
+        System.out.println(result);
+        result = Calculator.minus(10);
+        System.out.println(result);
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.divide(10));
+        System.out.println(calculator.sumAllOperation(10));
+    }
+
+
+}
