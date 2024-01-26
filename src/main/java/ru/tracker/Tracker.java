579c58f93acbcdcb04e3aaabe363b2f1df91e3f5
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-26 17:19:31 +0300

    9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +24,21 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                result[resultSize++] = item;
+            }
+        }
+        return Arrays.copyOf(result, resultSize);
     }
 }
