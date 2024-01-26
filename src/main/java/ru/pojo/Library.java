b362b3bdecce79ff8920fa81b72aa297dac940de
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-26 12:32:22 +0300

    9.3 Массивы и модели

---
+++ src/main/java/ru/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 67);
+        Book book2 = new Book("Winnie-the-Pooh", 250);
+        Book book3 = new Book("Jeeves and Wooster stories ", 145);
+        Book book4 = new Book("Harry Potter and the Philosopher’s Stone", 600);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getCountPages());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getCountPages());
+        }
+        for (Book book : books) {
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getCountPages());
+            }
+        }
+    }
+}
