b4d4d9dc02b12d4f1e2227832cea1e89c8c63715
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-27 14:24:00 +0300

    9.12 toString()

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -7,5 +7,8 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(formatter.format(item.getCreated()));
+        item.setId(1);
+        item.setName("First");
+        System.out.println(item);
     }
 }
