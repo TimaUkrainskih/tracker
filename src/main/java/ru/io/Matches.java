a68dad129d692954f66cd5898dfe80fb452b679a
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-28 13:25:39 +0300

    10.3 Чтение числа из консоли

---
+++ src/main/java/ru/io/Matches.java
@@ -0,0 +1,29 @@
+package ru.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Игра 11.");
+        boolean turn = true;
+        int count = 11;
+        while (count > 0) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            if (matches >= 1 && matches <= 3 && count >= matches) {
+                turn = !turn;
+                count -= matches;
+                System.out.println("На столе осталось " + count + " спичек");
+            } else {
+                System.out.println("Введенное число должно быть больше оставшегося количества спичек и находиться в диапазоне [1,3] ");
+            }
+        }
+        if (!turn) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
+        }
+    }
+}
