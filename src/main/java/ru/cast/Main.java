5826deeae98c7ac746906b51fccc7f373465fc7a
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-01-29 22:47:47 +0300

    10.15 Зачем нужно приведение типов

--- src/main/java/ru/cast/Main.java
+++ src/main/java/ru/cast/Main.java
@@ -2,13 +2,14 @@ package ru.cast;
 
 public class Main {
     public static void main(String[] args) {
-        Airplane airplane1 = new Airplane();
-        Airplane airplane2 = new Airplane();
-        Train train1 = new Train();
-        Train train2 = new Train();
-        Bus bus1 = new Bus();
-        Bus bus2 = new Bus();
+        Vehicle airplane1 = new Airplane();
+        Vehicle airplane2 = new Airplane();
+        Vehicle train1 = new Train();
+        Vehicle train2 = new Train();
+        Vehicle bus1 = new Bus();
+        Vehicle bus2 = new Bus();
         Vehicle[] vehicles = {airplane1, airplane2, train1, train2, bus1, bus2};
+
         for (Vehicle vehicle : vehicles) {
             System.out.println(vehicle.getClass());
             vehicle.move();
