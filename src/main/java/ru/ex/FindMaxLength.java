949a7a52d7548067115e8f060a53643b806fa2b3
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-01 13:17:31 +0300

    11.2 NullPointerExeption

--- src/main/java/ru/ex/FindMaxLength.java
+++ src/main/java/ru/ex/FindMaxLength.java
@@ -4,10 +4,9 @@ public class FindMaxLength {
     public static void main(String[] args) {
         String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
         int max = 0;
-        for (int index = 0; index < shops.length; index++) {
-            String element = shops[index];
-            if (element != null && element.length() > max) {
-                max = element.length();
+        for (String shop : shops) {
+            if (shop != null && shop.length() > max) {
+                max = shop.length();
             }
         }
         System.out.println("Max length : " + max);
