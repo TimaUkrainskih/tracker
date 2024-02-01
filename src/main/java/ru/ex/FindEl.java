a7e3be8af5fc5c5449b4b14551d1cf4ff58f84e9
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-01 17:09:30 +0300

    11.5 Пользовательские исключения

---
+++ src/main/java/ru/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ru.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                result = i;
+                break;
+            }
+        }
+        if (result != -1) {
+            return result;
+        } else {
+            throw new ElementNotFoundException("No such element");
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"Cat", "Dog", "Animal"};
+        try {
+            indexOf(words, "Bird");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
