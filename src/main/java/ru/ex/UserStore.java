51a8455b5a4891abbe9e7fd0476fa8fc5597a966
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-02 13:42:05 +0300

    11.6 Иерархия исключений

---
+++ src/main/java/ru/ex/UserStore.java
@@ -0,0 +1,36 @@
+package ru.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("User is not found");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Ivan Ivanov", true)
+        };
+        User user = null;
+        try {
+            user = findUser(users, "Ivan Ivanov");
+            validate(user);
+            System.out.println("This user has an access");
+        } catch (UserInvalidException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
