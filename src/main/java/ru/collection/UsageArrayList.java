e6e12f30d5480b575a5cd9cbf5993d0ff1443655
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-08 22:33:39 +0300

    13.2 ArrayList, List, Обобщения

--- src/main/java/ru/collection/UsageArrayList.java
+++ src/main/java/ru/collection/UsageArrayList.java
@@ -1,12 +1,10 @@
 package ru.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>(Arrays.asList("Petr", "Ivan", "Stepan"));
+        List<String> names = List.of("Petr", "Ivan", "Stepan");
         for (String name : names) {
             System.out.println(name);
         }
