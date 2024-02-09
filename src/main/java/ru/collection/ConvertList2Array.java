09abb949e1a235c46233040643c249a562fea81e
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-09 12:34:16 +0300

    13.6 ArrayList в двумерный массив

--- src/main/java/ru/collection/ConvertList2Array.java
+++ src/main/java/ru/collection/ConvertList2Array.java
@@ -10,9 +10,9 @@ public class ConvertList2Array {
         int row = 0;
         int cell = 0;
         for (Integer number : list) {
-            array[row][cell] = number;
-            cell = (cell + 1) % cells;
-            if (cell == 0) {
+            array[row][cell++] = number;
+            if (cell == cells) {
+                cell = 0;
                 row++;
             }
         }
