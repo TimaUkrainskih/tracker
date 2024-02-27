483c8ba60894acccfecd88c92bb6deb88f068eef
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-02-27 17:24:25 +0300

    23.1 Аттестация

---
+++ src/main/java/ru/stream/Analyze.java
@@ -0,0 +1,58 @@
+package ru.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .mapToDouble(Subject::score)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .map(pupil -> new Tuple(pupil.name(), pupil.subjects().stream()
+                        .mapToDouble(Subject::score)
+                        .average()
+                        .orElse(0.0)))
+                .toList();
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name, Collectors.averagingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream
+                .map(pupil -> new Tuple(pupil.name(), pupil.subjects()
+                        .stream()
+                        .mapToDouble(Subject::score)
+                        .sum()))
+                .max(Comparator.comparingDouble(Tuple::score))
+                .orElse(new Tuple("", 0D));
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name,
+                        Collectors.summingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingDouble(Map.Entry::getValue))
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .orElse(new Tuple("", 0D));
+    }
+}
